f68903b52500b6e86241372c2ea9be39664023a2
Диана <diana_yudina@mail.ru>
2022-04-13 21:35:46 +0500

    Task is done.

--- src/pro/sky/java/course5/Main.java
+++ src/pro/sky/java/course5/Main.java
@@ -1,8 +1,24 @@
 package pro.sky.java.course5;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+// Дан массив строк, необходимо удалить из него все дубли,
+// оставшиеся строки объединить в одну в порядке следования в массиве.
+
+        String[] strings = {"I", "wanna", "be", "loved", "by", "you", "just", "you", "And", "nobody", "else", "but",
+                "you", "I", "wanna", "be", "loved", "by", "you", "alone!"};
+
+        Set<String> set = new LinkedHashSet<>(Arrays.asList(strings));
+        String[] result = set.toArray(new String[0]);
+
+        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.toString(result));
+
+
     }
 }
