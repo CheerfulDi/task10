4be5b55fd0a37f5705f56eeadfea858922ad1d02
Диана <diana_yudina@mail.ru>
2022-04-14 22:45:21 +0500

    Task is done. Corrected.

--- src/pro/sky/java/course5/Main.java
+++ src/pro/sky/java/course5/Main.java
@@ -2,7 +2,9 @@ package pro.sky.java.course5;
 
 import java.util.Arrays;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -13,11 +15,20 @@ public class Main {
         String[] strings = {"I", "wanna", "be", "loved", "by", "you", "just", "you", "And", "nobody", "else", "but",
                 "you", "I", "wanna", "be", "loved", "by", "you", "alone!"};
 
+        System.out.println(Arrays.toString(strings));
+
+        //Первое решение
         Set<String> set = new LinkedHashSet<>(Arrays.asList(strings));
         String[] result = set.toArray(new String[0]);
-
-        System.out.println(Arrays.toString(strings));
         System.out.println(Arrays.toString(result));
+
+        //Второе решение
+
+        List<String> list = Arrays.stream(strings).distinct().toList();
+        System.out.println(list);
+
+
+
 
 
     }
